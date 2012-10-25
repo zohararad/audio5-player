3e7b1ecc3b14ee7eb9578df3dd8b15f946038287
Zohar Arad <zohar@zohararad.com>
2012-10-25 07:02:12 +0200

    added some function documentation to code

--- js/modules/audio_player.js
+++ js/modules/audio_player.js
@@ -1,4 +1,4 @@
-define(["../util/module", "../util/pubsub"], function(module, pubsub) {
+define(["../util/mixin", "../util/pubsub"], function(mixin, pubsub) {
   "use strict";
 
   var AudioPlayer = function(){
@@ -16,44 +16,76 @@ define(["../util/module", "../util/pubsub"], function(module, pubsub) {
   };
 
   AudioPlayer.prototype = {
-    src: null,
-    duration: 0,
-    position: 0,
-    volume: 100,
-    timer: null,
-    state: null,
+    src: null, //audio source
+    volume: 100, //audio volume
+    timer: null, //download interval timer
+    state: null, //player state
+    /**
+     * Load a new audio file into the player
+     * @param [String] src source of the audio to play
+     */
     load: function(src) {
       this.audio.src = this.src = src;
       this.timer = setInterval(this.checkDownloadProgress.bind(this), 500);
     },
+    /**
+     * Play current audio
+     */
     play: function() {
       this.audio.play();
     },
+    /**
+     * Pause current audio
+     */
     pause: function(){
       this.audio.pause();
     },
+    /**
+     * Stop current audio (pause and reset position to 0)
+     */
     stop: function(){
       this.pause();
       try{
-        this.audio.currentTime = 0;
+        this.seek(0);
       } catch(e) {}
     },
+    /**
+     * Seek to a given playback time
+     * @param [Integer] position playback position to seek to (in ms)
+     */
     seek: function(position){
       this.audio.currentTime = position / 1000;
       this.play();
     },
+    /**
+     * Playback completion handler
+     */
     complete: function(){
       this.clearTimer();
     },
+    /**
+     * Generic handler for DOM events on the audio object.
+     * Dispatches events to listeners and updates player state variable
+     * @param [DOMEvent] evt DOM event triggered on the audio object
+     */
     onPlayerEvent: function(evt){
       this.state = evt.type;
       this.trigger(evt.type, this);
     },
+    /**
+     * Event handler for playback time change.
+     * Triggers 'playTimeUpdate' events with current play time and playback duration.
+     */
     onTimeUpdate: function(){
       if(this.audio.buffered.length > 0 && this.audio.duration > 0){
         this.trigger('playTimeUpdate', this.audio.currentTime * 1000, this.audio.duration * 1000);
       }
     },
+    /**
+     * Checks track download progress by the browser.
+     * Called periodically after track is loaded to the audio object.
+     * Triggers 'downloadProgress' event with percent of loaded audio
+     */
     checkDownloadProgress: function(){
       var loaded = parseInt(((this.audio.buffered.end(this.audio.buffered.length-1) / this.audio.duration) * 100), 10);
       this.trigger('downloadProgress', loaded);
@@ -61,6 +93,9 @@ define(["../util/module", "../util/pubsub"], function(module, pubsub) {
         this.clearTimer();
       }
     },
+    /**
+     * Clear periodical download progress timer
+     */
     clearTimer: function(){
       if(this.timer !== null){
         clearInterval(this.timer);
@@ -69,6 +104,6 @@ define(["../util/module", "../util/pubsub"], function(module, pubsub) {
     }
   };
 
-  module.include(AudioPlayer, pubsub);
+  mixin.include(AudioPlayer, pubsub);
   return AudioPlayer;
 });
--- js/util/pubsub.js
+++ js/util/pubsub.js
@@ -1,9 +1,22 @@
+/**
+ * Defines a pub/sub object that can be mixed into other classes / objects
+ */
 define({
-  channels: {},
+  channels: {}, //hash of subscribed channels
+  /**
+   * Subscribe to event on a channel
+   * @param [String] evt name of channel / event to subscribe
+   * @param [Function] fn the callback to execute on message publishing
+   * @param [Object] ctx the context in which the callback should be executed
+   */
   on: function(evt, fn, ctx){
     this.channels[evt] = this.channels[evt] || [];
     this.channels[evt].push({fn: fn, ctx: ctx});
   },
+  /**
+   * Publish a message on a channel. Accepts **args after event name
+   * @param [String] evt name of channel / event to trigger
+   */
   trigger: function(evt){
     if(this.channels.hasOwnProperty(evt)){
       var args = Array.prototype.slice.call(arguments, 1);
