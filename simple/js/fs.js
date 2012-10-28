8aaa82dd0fd4b699e52883fa7e693755fc51f6b0
Zohar Arad <zohar@zohararad.com>
2012-10-28 20:43:29 +0200

    added comments to FS module

--- simple/js/fs.js
+++ simple/js/fs.js
@@ -2,10 +2,17 @@
 (function($){
   "use strict";
 
+  // normalize vendor-prefixed functionality
   window.requestFileSystem  = window.requestFileSystem || window.webkitRequestFileSystem;
   window.resolveLocalFileSystemURL = window.resolveLocalFileSystemURL || window.webkitResolveLocalFileSystemURL;
 
+  /**
+   * File System API Utility class
+   * @param [Function] cb callback function to run when file system API is ready
+   * @constructor
+   */
   var FS = function(cb){
+    // Request persistent storage quota from FS API
     window.webkitStorageInfo.requestQuota(
       PERSISTENT,
       50*1024*1024,
@@ -14,7 +21,13 @@
   }
 
   FS.prototype = {
-    rootDir: '/audio_files',
+    rootDir: '/audio_files', //root directory to store files in
+    /**
+     * Handler for quota grant event.
+     * Requests file-system access once quota is granted
+     * @param [Function] cb callback function to run when file system API is ready
+     * @param [Integer] bytes granted quota in bytes
+     */
     onQuotaGranted: function(cb, bytes){
       window.requestFileSystem(
         PERSISTENT,
@@ -23,10 +36,19 @@
         this.onFSError.bind(this)
       );
     },
+    /**
+     * Files sytem ready event handler
+     * @param [Function] cb callback function to run when file system API is ready
+     * @param [FileSystem] fs reference to file system object
+     */
     onFSReady: function(cb, fs){
       this.fs = fs;
       cb();
     },
+    /**
+     * File system errors event handler
+     * @param [Error] err error object for file system error events
+     */
     onFSError: function(err){
       var msg = '';
       switch (err.code) {
@@ -52,28 +74,45 @@
 
       console.log('Error: ' + msg);
     },
+    /**
+     * Write file to file system
+     * @param [File] f file to write
+     * @param [Function] cb callback function to run when file has been successfully written
+     */
     write: function(f, cb){
+      var that = this;
       this.fs.root.getDirectory(this.rootDir, {create: true}, function(dirEntry) {
-        var fileName = [this.rootDir, f.name].join('/');
-        this.fs.root.getFile(fileName, {create: true, exclusive: false}, function(fileEntry) {
+        var fileName = [that.rootDir, f.name].join('/');
+        that.fs.root.getFile(fileName, {create: true, exclusive: false}, function(fileEntry) {
           fileEntry.createWriter(function(fileWriter) {
             fileWriter.write(f);
             cb(fileEntry);
-          }.bind(this), this.onFSError);
-        }.bind(this), this.onFSError);
-      }.bind(this), this.onFSError);
+          }, that.onFSError);
+        }, that.onFSError);
+      }, that.onFSError);
     },
+    /**
+     * Get all file entries stored in `this.rootDir`
+     * @param [Function] cb callback function to run when all file entries have been read
+     */
     all: function(cb){
       this.fs.root.getDirectory(this.rootDir, {create: true}, function(dirEntry) {
         var dirReader = dirEntry.createReader();
         dirReader.readEntries(function(results) {
           cb($.toArray(results));
         });
-      }.bind(this));
+      });
     },
+    /**
+     * Remove a file from the file system
+     * @param [FileEntry] fileEntry file entry to remove
+     * @param [Function] cb callback function to run when file has been successfully removed
+     */
     remove: function(fileEntry, cb){
       fileEntry.remove(cb, this.onFSError);
     }
   }
+
+  // export as global module;
   window.FS = FS;
 })(jQuery);
