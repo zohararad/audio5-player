845404c435f6c1a7cfad384f27aae8eea9d3237a
Zohar Arad <zohar@zohararad.com>
2012-10-28 19:35:01 +0200

    added player controller and audio classes

--- simple/js/player.js
+++ simple/js/player.js
@@ -63,6 +63,7 @@
       setTimeout(f, 5);
     },
     getFileID3: function(data){
+      // see http://ericbidelman.tumblr.com/post/8343485440/reading-mp3-id3-tags-in-javascript for more info
       var dv = new jDataView(data);
 
       var o = {title: null, artist: null, album: null, year: null};
@@ -127,7 +128,6 @@
       this.dom.playlist.find('tr[data-name="'+name+'"]').remove();
     },
     getItemHTML: function(item){
-      console.log(item);
       var tds = [];
       ['title', 'artist', 'album', 'year'].forEach(function(k){
         tds.push('<td>'+item[k]+'</td>');
@@ -137,20 +137,46 @@
     }
   }
 
-  var Player = function(container){
-    this.init(container);
+  var Player = {
+    Audio: function(container){
+      this.init(container);
+    },
+    View:  function(container){
+      this.init(container);
+    },
+    Controller: function(container){
+      this.init(container);
+    }
   }
 
-  Player.prototype = {
+  Player.Audio.prototype = {
+    init: function(container){
+      this.audio = container.find('audio').get(0);
+      this.audio.preload = 'metadata';
+      this.audio.autoplay = false;
+    },
+    load: function(url){
+      this.audio.src = url;
+      this.play();
+    },
+    play: function(){
+      this.audio.play();
+    },
+    pause: function(){
+      this.audio.pause();
+    }
+  }
+
+  Player.Controller.prototype = {
     dom:{},
     init: function(container){
       this.dom.container = container;
       this.dom.browse = this.dom.container.find('input[type=file]');
-      this.dom.audio = this.dom.container.find('audio').get(0);
       this.dom.playlist = this.dom.container.find('.playlist tbody');
 
       this.view = new Playlist.View(this.dom.playlist);
       this.model = new Playlist.Model(this.render.bind(this));
+      this.audio = new Player.Audio(container);
     },
     render: function(){
       var that = this;
@@ -173,7 +199,15 @@
       }
       evt.target.value = '';
     },
-    onTrackClick: function(){},
+    onTrackClick: function(evt){
+      var tr = $(evt.target);
+      if(tr.get('tag') !== 'TR'){
+        tr = tr.parents('tr');
+      }
+      var name = tr.data('name');
+      var track = this.model.find(name);
+      this.audio.load(track.url);
+    },
     onRemoveTrackClick: function(evt){
       evt.stopPropagation();
       var s = $(evt.target);
@@ -185,152 +219,5 @@
     }
   }
 
-  var OldPlayer = function(container){
-    this.init(container);
-  }
-
-  OldPlayer.prototype = {
-    dom:{},
-    fileList: [],
-    init: function(container){
-      this.dom.container = container;
-      this.fs = new FS(this.setup.bind(this));
-    },
-    setup: function(){
-
-      this.dom.browse = this.dom.container.find('input[type=file]');
-      this.dom.audio = this.dom.container.find('audio').get(0);
-      this.dom.playlist = this.dom.container.find('.playlist tbody');
-      this.dom.browse.on('change', this.onFilePick.bind(this));
-      this.dom.playlist.delegate('tr','click',this.onTrackClick.bind(this));
-      this.dom.playlist.delegate('button[data-action=remove]','click',this.onRemoveTrackClick.bind(this));
-      this.fs.all(function(results){
-        results.forEach(function(fileEntry){
-          if(this.getTrackIndex(fileEntry.name) === -1){
-            this.addFileEntry(fileEntry, this.addToPlaylist.bind(this, fileEntry));
-          }
-        }.bind(this));
-      }.bind(this));
-    },
-    onFilePick: function(evt){
-      var files = evt.target.files; // FileList object
-      for (var i = 0, f; f = files[i]; i++) {
-        this.handleFilePick(f);
-      }
-      evt.target.value = '';
-    },
-    handleFilePick: function(file){
-      if(this.getTrackIndex(file.name) === -1){
-        this.fs.write(file, function(fileEntry){
-          setTimeout(function(){
-            this.addFileEntry(fileEntry, this.addToPlaylist.bind(this, fileEntry));
-          }.bind(this), 5);
-        }.bind(this));
-      }
-    },
-    addFileEntry: function(fileEntry, cb){
-      // see http://ericbidelman.tumblr.com/post/8343485440/reading-mp3-id3-tags-in-javascript for more info
-      fileEntry.file(function(file){
-        console.log(fileEntry, file);
-        var reader = new FileReader();
-
-        reader.onloadend = function(evt) {
-          var dv = new jDataView(evt.target.result);
-
-          var title, artist, album, year;
-
-          if (dv.getString(3, dv.byteLength - 128) == 'TAG') {
-            title = dv.getString(30, dv.tell());
-            artist = dv.getString(30, dv.tell());
-            album = dv.getString(30, dv.tell());
-            year = dv.getString(4, dv.tell());
-          }
-
-          this.fileList.push({
-            file: fileEntry,
-            name: fileEntry.name,
-            title: title,
-            artist: artist,
-            album: album,
-            year: year
-          });
-          if(cb !== undefined){
-            cb();
-          }
-        }.bind(this);
-
-        // Read in the image file as a data URL.
-        reader.readAsArrayBuffer(file);
-
-      }.bind(this));
-    },
-    renderPlayList: function(){
-      var html = [];
-      this.fileList.forEach(function(f){
-        html.push(this.getTrackListItemHTML(f));
-      }.bind(this));
-      this.dom.playlist.html(html.join(''));
-    },
-    addToPlaylist: function(fileEntry){
-      var index = this.getTrackIndex(fileEntry.name);
-      if(index > -1){
-        var f = this.fileList[index];
-        var li = $(this.getTrackListItemHTML(f));
-        li.appendTo(this.dom.playlist);
-      }
-    },
-    getTrackListItemHTML: function(f){
-      var tds = [];
-      ['title', 'artist', 'album', 'year'].forEach(function(k){
-        tds.push('<td>'+f[k]+'</td>');
-      });
-      tds.push('<td><button type="button" data-action="remove" data-name="'+ f.name +'">Remove</button></td>');
-      return '<tr class="track" data-name="'+ f.name +'">' + tds.join('') + '</tr>';
-    },
-    onTrackClick: function(evt){
-      var tr = $(evt.target);
-      if(tr.get('tag') !== 'TR'){
-        tr = tr.parents('tr');
-      }
-      var name = tr.data('name');
-      var index = this.getTrackIndex(name);
-      if(index > -1){
-        this.playFromList(index);
-      }
-    },
-    onRemoveTrackClick: function(evt){
-      evt.stopPropagation();
-      var s = $(evt.target);
-      var name = s.data('name');
-      var index = this.getTrackIndex(name);
-      if(index > -1){
-        this.fs.remove(this.fileList[index].file, this.onTrackRemove.bind(this, index));
-      }
-    },
-    onTrackRemove: function(index){
-      var f = this.fileList[index];
-      this.dom.playlist.find('tr[data-name="'+ f.name +'"]').remove();
-      this.fileList.splice(index, 1);
-    },
-    playFromList: function(index){
-      var f = this.fileList[index];
-      this.dom.audio.src = f.file.toURL();
-      this.dom.audio.play();
-    },
-    getTrackIndex: function(name){
-      return $.indexOfMemberByAttr(this.fileList, 'name', name);
-    },
-    sortTracks: function(field, dir){
-      this.fileList.sort(function(a,b){
-        if(dir === 'asc'){
-          return a[field] > b[field] ? 1 : (a[field] < b[field] ? -1 : 0);
-        } else {
-          return b[field] > a[field] ? 1 : (b[field] < a[field] ? -1 : 0);
-        }
-      });
-      this.renderPlayList();
-    }
-  }
-
-  $win.player = new Player($('#player'))
+  $win.player = new Player.Controller($('#player'))
 })(this, jQuery);
