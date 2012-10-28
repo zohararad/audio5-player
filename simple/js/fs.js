1b51077613cc1155f8f2ba4189edd868b26f359e
Zohar Arad <zohar@zohararad.com>
2012-10-28 20:24:02 +0200

    remove toArray helper from FS module

--- simple/js/fs.js
+++ simple/js/fs.js
@@ -2,10 +2,6 @@
 (function($){
   "use strict";
 
-  function toArray(list) {
-    return Array.prototype.slice.call(list || [], 0);
-  }
-
   window.requestFileSystem  = window.requestFileSystem || window.webkitRequestFileSystem;
   window.resolveLocalFileSystemURL = window.resolveLocalFileSystemURL || window.webkitResolveLocalFileSystemURL;
 
