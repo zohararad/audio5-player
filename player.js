8b30409e57fd60cf22c18dd697d7ed1530e0bb5f
Zohar Arad <zohar@zohararad.com>
2012-11-10 14:31:14 +0200

    add playbackRate basics

--- player.js
+++ player.js
@@ -1,5 +1,6 @@
 var input = document.querySelector('input[type=file]');
 var audio = document.querySelector('audio');
+var range = document.querySelector('input[type=range]');
 
 input.addEventListener('change', function(event){
 	var file = event.target.files[0];
@@ -10,3 +11,7 @@ input.addEventListener('change', function(event){
 	};
 	reader.readAsDataURL(file);
 }, false);
+
+range.addEventListener('change', function(event){
+  audio.playbackRate = event.target.value;
+}, false)
