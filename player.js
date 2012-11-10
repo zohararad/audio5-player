33a1033a136e0907f5d4b4f90407217e391c0ac3
Ronny Orbach <r@ronnyo.com>
2012-11-10 14:13:05 +0200

    new, simpler files

---
+++ player.js
@@ -0,0 +1,9 @@
+var input = document.querySelector('input[type=file]');
+input.addEventListener('change', function(event){
+	var file = event.target.files[0];
+	var reader = new FileReader();
+	reader.onload = function(){
+		console.log(arguments);
+	};
+	console.log(reader);
+}, false);
