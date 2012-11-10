ad348e193719d1c4403bc1116cd84c250222472e
Zohar Arad <zohar@zohararad.com>
2012-11-10 14:24:18 +0200

    add audio playback from dataURI

--- player.js
+++ player.js
@@ -1,9 +1,12 @@
 var input = document.querySelector('input[type=file]');
+var audio = document.querySelector('audio');
+
 input.addEventListener('change', function(event){
 	var file = event.target.files[0];
 	var reader = new FileReader();
-	reader.onload = function(){
-		console.log(arguments);
+	reader.onload = function(e){
+		audio.src = e.target.result;
+    audio.play();
 	};
 	reader.readAsDataURL(file);
 }, false);
